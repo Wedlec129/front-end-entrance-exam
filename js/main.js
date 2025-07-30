32337ba9ffef3f86fb28afe8a5a04b4c0989d0c5
Wedlec129 <boryasokolov@gmail.com>
2025-07-30 15:34:03 +0300

    add languages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import '../css/style.css'
 import {createProfileSection} from "./components/photo-profile";
 import {createNameSection} from "./components/name";
+import {createLangsSection} from "./components/languages";
  
 
 document.querySelector('#app').innerHTML =[
   createProfileSection(),
-  createNameSection()
+  createNameSection(),
+  createLangsSection()
 ].join('\n')
