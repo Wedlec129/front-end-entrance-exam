d8baa6c3049bdce0a615e487c5775a3e4eb51242
Wedlec129 <boryasokolov@gmail.com>
2025-07-30 15:32:53 +0300

    add name

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import '../css/style.css'
 import {createProfileSection} from "./components/photo-profile";
+import {createNameSection} from "./components/name";
  
 
 document.querySelector('#app').innerHTML =[
-  createProfileSection()
+  createProfileSection(),
+  createNameSection()
 ].join('\n')
